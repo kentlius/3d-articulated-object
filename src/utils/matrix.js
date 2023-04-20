e255f9081bd0bf2a5c73c0c1cdb6511d1c02a76a
Audino723 <rioaudino723@gmail.com>
2023-04-20 19:35:00 +0700

    refactor matrix

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -1,70 +1,36 @@
 import { subtractVectors, normalize, cross } from "./utility.js";
 
 export class Matrix4 {
-  nRow = 4;
-  nCol = 4;
-  matrix;
+  elements;
 
   constructor(matrix) {
-    this.matrix = new Array(this.nRow * this.nCol);
-
-    // Check that matrix is compatible.
-    if (matrix != null && matrix.length == this.nRow * this.nCol) {
-      this.matrix = matrix;
+    this.elements = new Array(16);
+    if (matrix != null && matrix.length == 16) {
+      this.elements = matrix;
     }
   }
 
-  /**
-   * Set the value of the matrix at the given row and column. 
-   *
-   * @param row - The row of the matrix.
-   * @param col - The column of the matrix.
-   * @param value - The value to set.
-   */
   set(row, col, value) {
-    this.matrix[row * this.nCol + col] = value;
+    this.elements[row * 4 + col] = value;
   }
 
-  /**
-   * Get the value of the matrix at the given row and column. 
-   *
-   * @param row - The row of the matrix.
-   * @param col - The column of the matrix.
-   * @returns The value of the matrix at the given row and column.
-   */
-  get(row, col) {
-    return this.matrix[row * this.nCol + col];
-  }
-
-  /**
-   * Set the array of matrix. 
-   *
-   * @param matrix - The array of matrix.
-   */
   setMatrix(matrix) {
-    this.matrix = matrix;
+    this.elements = matrix;
   }
 
-  /**
-   * Get the array of matrix. 
-   *
-   * @returns - The array of matrix.
-   */
+  get(row, col) {
+    return this.elements[row * 4 + col];
+  }
+
   getMatrix() {
-    return this.matrix;
+    return this.elements;
   }
 
-  /**
-   * Clone the matrix.
-   *
-   * @param matrix - The matrix to be cloned.
-   * @returns - The cloned matrix.
-   */
   clone = () => {
     let result = new Matrix4();
 
-    for (let row = 0; row < this.nRow; row++) {
-      for (let col = 0; col < this.nCol; col++) {
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
         result.set(row, col, this.get(row, col));
       }
     }
@@ -72,167 +38,201 @@ export class Matrix4 {
     return result;
   }
 
-  /**
-   * Create a identity matrix. 
-   *
-   * @returns - The identity matrix.
-   */
   static identity = () => {
-    let matrix = new Matrix4();
-
-    for (let row = 0; row < matrix.nRow; row++) {
-      for (let col = 0; col < matrix.nCol; col++) {
-        if (row === col) {
-          matrix.set(row, col, 1);
-        } else {
-          matrix.set(row, col, 0);
-        }
-      }
-    }
+    let identity_matrix = [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]
+    let matrix = new Matrix4(identity_matrix);
     return matrix;
   }
 
-  /**
-   * Transpose the matrix and save the result in this matrix.
-   *
-   */
   transpose = () => {
-    for (let row = 0; row < this.nRow; row++) {
-      for (let col = row; col < this.nCol; col++) {
-        let tmp = this.get(row, col);
-        this.set(row, col, this.get(col, row));
-        this.set(col, row, tmp);
-      }
-    }
+    var transpose_matrix = [
+        this.elements[0], this.elements[4], this.elements[8], this.elements[12],
+        this.elements[1], this.elements[5], this.elements[9], this.elements[13],
+        this.elements[2], this.elements[6], this.elements[10], this.elements[14],
+        this.elements[3], this.elements[7], this.elements[11], this.elements[15],
+    ]
+
+    this.setMatrix(transpose_matrix);
   }
 
-  /**
-   * Multiply the matrix with this matrix and save the result in this matrix.
-   * Example: [matrix]*[this] 
-   *
-   * @param matrix - The matrix to multiply.
-   */
   multiply = (matrix) => {
     let result = new Matrix4();
-    for (let row = 0; row < this.nRow; row++) {
-      for (let col = 0; col < this.nCol; col++) {
-        let sum = 0;
-        for (let i = 0; i < this.nCol; i++) {
-          sum += matrix.get(row, i) * this.get(i, col);
-        }
-        result.set(row, col, sum);
-      }
-    }
-    this.setMatrix(result.getMatrix());
+
+    var a = this.getMatrix();
+    var b = matrix.getMatrix();
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
+    var result_matrix = [
+        b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+        b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+        b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+        b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+        b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+        b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+        b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+        b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+        b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+        b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+        b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+        b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+        b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+        b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+        b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+        b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+    ];
+
+    this.setMatrix(result_matrix);
   }
 
-  /**
-   * Multiply the matrix with this matrix and save the result into new matrix.
-   * Example: [a]*[b] 
-   *
-   * @param matrix - The matrix to multiply.
-   * @returns - The new matrix.
-   */
-  static multiply = (a, b) => {
+  static multiply = (matrix_a, matrix_b) => {
     let result = new Matrix4();
-    for (let row = 0; row < a.nRow; row++) {
-      for (let col = 0; col < a.nCol; col++) {
-        let sum = 0;
-        for (let i = 0; i < a.nCol; i++) {
-          sum += a.get(row, i) * b.get(i, col);
-        }
-        result.set(row, col, sum);
-      }
-    }
+
+    var a = matrix_a.getMatrix();
+    var b = matrix_b.getMatrix();
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
+    var result_matrix = [
+        b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+        b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+        b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+        b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+        b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+        b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+        b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+        b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+        b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+        b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+        b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+        b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+        b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+        b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+        b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+        b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+    ];
+
+    result.setMatrix(result_matrix);
     return result;
   }
 
-  /**
-   * Inverse this matrix and save the result in this matrix.
-   */
   inverse = () => {
-    let m00 = this.matrix[0 * 4 + 0];
-    let m01 = this.matrix[0 * 4 + 1];
-    let m02 = this.matrix[0 * 4 + 2];
-    let m03 = this.matrix[0 * 4 + 3];
-    let m10 = this.matrix[1 * 4 + 0];
-    let m11 = this.matrix[1 * 4 + 1];
-    let m12 = this.matrix[1 * 4 + 2];
-    let m13 = this.matrix[1 * 4 + 3];
-    let m20 = this.matrix[2 * 4 + 0];
-    let m21 = this.matrix[2 * 4 + 1];
-    let m22 = this.matrix[2 * 4 + 2];
-    let m23 = this.matrix[2 * 4 + 3];
-    let m30 = this.matrix[3 * 4 + 0];
-    let m31 = this.matrix[3 * 4 + 1];
-    let m32 = this.matrix[3 * 4 + 2];
-    let m33 = this.matrix[3 * 4 + 3];
-    let tmp_0  = m22 * m33;
-    let tmp_1  = m32 * m23;
-    let tmp_2  = m12 * m33;
-    let tmp_3  = m32 * m13;
-    let tmp_4  = m12 * m23;
-    let tmp_5  = m22 * m13;
-    let tmp_6  = m02 * m33;
-    let tmp_7  = m32 * m03;
-    let tmp_8  = m02 * m23;
-    let tmp_9  = m22 * m03;
-    let tmp_10 = m02 * m13;
-    let tmp_11 = m12 * m03;
-    let tmp_12 = m20 * m31;
-    let tmp_13 = m30 * m21;
-    let tmp_14 = m10 * m31;
-    let tmp_15 = m30 * m11;
-    let tmp_16 = m10 * m21;
-    let tmp_17 = m20 * m11;
-    let tmp_18 = m00 * m31;
-    let tmp_19 = m30 * m01;
-    let tmp_20 = m00 * m21;
-    let tmp_21 = m20 * m01;
-    let tmp_22 = m00 * m11;
-    let tmp_23 = m10 * m01;
+    var a00 = this.elements[0 * 4 + 0];
+    var a01 = this.elements[0 * 4 + 1];
+    var a02 = this.elements[0 * 4 + 2];
+    var a03 = this.elements[0 * 4 + 3];
+    var a10 = this.elements[1 * 4 + 0];
+    var a11 = this.elements[1 * 4 + 1];
+    var a12 = this.elements[1 * 4 + 2];
+    var a13 = this.elements[1 * 4 + 3];
+    var a20 = this.elements[2 * 4 + 0];
+    var a21 = this.elements[2 * 4 + 1];
+    var a22 = this.elements[2 * 4 + 2];
+    var a23 = this.elements[2 * 4 + 3];
+    var a30 = this.elements[3 * 4 + 0];
+    var a31 = this.elements[3 * 4 + 1];
+    var a32 = this.elements[3 * 4 + 2];
+    var a33 = this.elements[3 * 4 + 3];
 
-    let t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
-        (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
-    let t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
-        (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
-    let t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
-        (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
-    let t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
-        (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+    var b00 = a00 * a11 - a01 * a10;
+    var b01 = a00 * a12 - a02 * a10;
+    var b02 = a00 * a13 - a03 * a10;
+    var b03 = a01 * a12 - a02 * a11;
+    var b04 = a01 * a13 - a03 * a11;
+    var b05 = a02 * a13 - a03 * a12;
+    var b06 = a20 * a31 - a21 * a30;
+    var b07 = a20 * a32 - a22 * a30;
+    var b08 = a20 * a33 - a23 * a30;
+    var b09 = a21 * a32 - a22 * a31;
+    var b10 = a21 * a33 - a23 * a31;
+    var b11 = a22 * a33 - a23 * a32;
 
-    let d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+    var invDet = 1 / (b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06);
 
-    this.setMatrix([
-      d * t0,
-      d * t1,
-      d * t2,
-      d * t3,
-      d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
-            (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
-      d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
-            (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
-      d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
-            (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
-      d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
-            (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
-      d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
-            (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
-      d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
-            (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
-      d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
-            (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
-      d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
-            (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
-      d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
-            (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
-      d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
-            (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
-      d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
-            (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
-      d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
-            (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
-    ]);
+    var inv_matrix = [
+      (a11 * b11 - a12 * b10 + a13 * b09) * invDet,
+      (a02 * b10 - a01 * b11 - a03 * b09) * invDet,
+      (a31 * b05 - a32 * b04 + a33 * b03) * invDet,
+      (a22 * b04 - a21 * b05 - a23 * b03) * invDet,
+      (a12 * b08 - a10 * b11 - a13 * b07) * invDet,
+      (a00 * b11 - a02 * b08 + a03 * b07) * invDet,
+      (a32 * b02 - a30 * b05 - a33 * b01) * invDet,
+      (a20 * b05 - a22 * b02 + a23 * b01) * invDet,
+      (a10 * b10 - a11 * b08 + a13 * b06) * invDet,
+      (a01 * b08 - a00 * b10 - a03 * b06) * invDet,
+      (a30 * b04 - a31 * b02 + a33 * b00) * invDet,
+      (a21 * b02 - a20 * b04 - a23 * b00) * invDet,
+      (a11 * b07 - a10 * b09 - a12 * b06) * invDet,
+      (a00 * b09 - a01 * b07 + a02 * b06) * invDet,
+      (a31 * b01 - a30 * b03 - a32 * b00) * invDet,
+      (a20 * b03 - a21 * b01 + a22 * b00) * invDet,
+    ]
+
+    this.setMatrix(inv_matrix);
+
     return this;
   }
 
@@ -261,17 +261,6 @@ export class Matrix4 {
       ])
   }
 
-  /**
-   * Create a orthographic projection matrix. 
-   *
-   * @param left - The left value of the projection.
-   * @param right - The right value of the projection.
-   * @param bottom - The bottom value of the projection.
-   * @param top - The top value of the projection.
-   * @param near - The near value of the projection.
-   * @param far - The far value of the projection.
-   * @returns - The orthographic projection matrix.
-   */
   static orthographic = (left, right, bottom, top, near, far) => {
     let width = right - left;
     let height = top - bottom;
@@ -285,15 +274,6 @@ export class Matrix4 {
     ]);
   }
 
-  /**
-   * Create a perspective projection matrix. 
-   *
-   * @param fov - The field of view of the projection.
-   * @param aspect - The aspect of the projection.
-   * @param near - The near value of the projection.
-   * @param far - The far value of the projection.
-   * @returns - The perspective projection matrix.
-   */
   static perspective = (fov, aspect, near, far) => {
     let f = Math.tan(Math.PI * 0.5 - 0.5 * fov);
     let rangeInv = 1.0 / (near - far);
@@ -306,14 +286,7 @@ export class Matrix4 {
     ]);
   }
 
-  /**
-   * Create a oblique projection matrix. 
-   *
-   * @param theta - The angle between object and projection plane
-   * @param phi - The angle between object and x axis
-   * @returns - The oblique projection matrix.
-   */
-   static oblique = (theta, phi) => {
+  static oblique = (theta, phi) => {
     let t = theta * Math.PI / 180;
     let p = phi * Math.PI / 180;
 
@@ -332,7 +305,6 @@ export class Matrix4 {
     return matrix;
   }
 
-
   static projection(width, height, depth) {
     // Note: This matrix flips the Y axis so 0 is at the top.
     return new Matrix4([
@@ -343,12 +315,6 @@ export class Matrix4 {
     ]);
   }
 
-
-  /**
-   * Rotate the matrix in x-axis.
-   *
-   * @param angle - The angle of rotation in radian.
-   */
   rotateX = (angle) => {
     let matrix = new Matrix4();
     let s = Math.sin(angle);
@@ -363,12 +329,7 @@ export class Matrix4 {
 
     this.multiply(matrix);
   }
-
-  /**
-   * Rotate the matrix in y-axis. 
-   *
-   * @param angle - The angle of rotation in radian.
-   */
+ 
   rotateY = (angle) => {
     let matrix = new Matrix4();
     let s = Math.sin(angle);
@@ -384,11 +345,6 @@ export class Matrix4 {
     this.multiply(matrix);
   }
 
-  /**
-   * Rotate the matrix in z-axis. 
-   *
-   * @param angle - The angle of rotation in radian.
-   */
   rotateZ = (angle) => {
     let matrix = new Matrix4();
     let s = Math.sin(angle);
@@ -404,13 +360,6 @@ export class Matrix4 {
     this.multiply(matrix);
   }
 
-  /**
-   * Translate the matrix in direction of the given parameter value. 
-   *
-   * @param tx - The value of translation in x-axis. 
-   * @param ty - The value of translation in y-axis.
-   * @param tz - The value of translation in z-axis.
-   */
   translate = (tx, ty, tz) => {
     let matrix = new Matrix4();
     matrix.setMatrix([
@@ -424,13 +373,6 @@ export class Matrix4 {
     return this;
   }
 
-  /**
-   * Rotate the matrix in all axis. 
-   *
-   * @param angleX - The angle of rotation in x-axis (radian).
-   * @param angleY - The angle of rotation in y-axis (radian).
-   * @param angleZ - The angle of rotation in z-axis (radian).
-   */
   rotate = (angleX, angleY, angleZ) => {
     let matrix = Matrix4.identity();
     matrix.rotateX(angleX);
@@ -441,13 +383,6 @@ export class Matrix4 {
     return this;
   }
 
-  /**
-   * Scale the matrix in x-axis. 
-   *
-   * @param sx - The value of scaling in x-axis.
-   * @param sy - The value of scaling in y-axis.
-   * @param sz - The value of scaling in z-axis.
-   */
   scale = (sx, sy, sz) => {
     let matrix = new Matrix4();
     matrix.setMatrix([
@@ -461,13 +396,6 @@ export class Matrix4 {
     return this;
   } 
 
-  /**
-   * Update the matrix with the given parameters. 
-   *
-   * @param translation - The translation of the matrix.
-   * @param rotation - The rotation of the matrix.
-   * @param scale - The scale of the matrix.
-   */
   transform = (translation, rotation, scale) => {
     this.translate(translation[0], translation[1], translation[2]);
     this.rotate(rotation[0], rotation[1], rotation[2]);
